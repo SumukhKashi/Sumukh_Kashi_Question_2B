874e91987649f81459851ad7aa519a4cca55226b
Sumukh Kashi <sumukharkashi@gmail.com>
2024-01-02 12:36:02 +0530

    Initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Flask App!'
+    return 'Sumukh Kashi 1BM20IS163!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
