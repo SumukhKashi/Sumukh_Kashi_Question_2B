d278093e23a996f5f91f4229a404a76de5fa717b
Sumukh Kashi <sumukharkashi@gmail.com>
2024-01-02 12:34:03 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(_name_)
-
-# @app.route('/')
-# def index():
-#     return 'Hello, your_name (your_usn)!'
-
-# if _name_ == '_main_':
-#     app.run(debug=True, host='0.0.0.0')
-
-from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
-def home_page(name=None):
-    # return render_template("index.html",name=name)
-    print("Sumukh Kashi 1BM20IS163")
-
+@app.route('/')
+def hello():
+    return 'Hello, Flask App!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
